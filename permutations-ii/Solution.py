a3761e3e59f1899e378777ed940a517f2eb6f2a9
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-27 08:59:44 +0530

    feat: solve Permutations II

---
+++ permutations-ii/Solution.py
@@ -0,0 +1,7 @@
+from itertools import permutations 
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        p = list(permutations(nums)) 
+        s = set(p)  
+        ans = list(s)
+        return ans
