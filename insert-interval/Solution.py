8a50c5da16deebf1dfdeb060ae48d0bb1e08850e
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-28 08:55:11 +0530

    feat: solve Insert Interval

--- insert-interval/Solution.py
+++ insert-interval/Solution.py
@@ -1,19 +1,19 @@
 class Solution(object):
-    def insert(self, intervals, newInterval):
-        merged = []
+    def insert(self, ii, new):
+        m = []
         i = 0
 
-        while i < len(intervals) and intervals[i][1] < newInterval[0]:
-            merged.append(intervals[i])
+        while i < len(ii) and ii[i][1] < new[0]:
+            m.append(ii[i])
             i += 1
         
-        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
-            newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        while i < len(ii) and ii[i][0] <= new[1]:
+            new = [min(new[0], ii[i][0]), max(new[1], ii[i][1])]
             i += 1
-        merged.append(newInterval)
+        m.append(new)
         
-        while i < len(intervals):
-            merged.append(intervals[i])
+        while i < len(ii):
+            m.append(ii[i])
             i += 1
         
-        return merged
+        return m
