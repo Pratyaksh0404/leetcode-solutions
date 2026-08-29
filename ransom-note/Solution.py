88cbb851f8d12c8b707fe0aaf27de447cef11952
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-29 09:39:24 +0530

    feat: solve Ransom Note

--- ransom-note/Solution.py
+++ ransom-note/Solution.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def canConstruct(self, rn, mag):
         st1, st2 = Counter(rn), Counter(mag)
+
         if st1 & st2 == st1:
             return True
+            
         return False
