9021257e31a4b6dfdaa4ba27f08eba067ff8007f
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-31 21:25:42 +0530

    feat: solve Valid Perfect Square

--- valid-perfect-square/Solution.py
+++ valid-perfect-square/Solution.py
@@ -1,16 +1,13 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        left = 1
-        right = num
-
-        while left <= right:
-            mid = left + (right - left) // 2
-
+        l, r = 1, num
+        while l <= r:
+            mid = l + (r - l) // 2
             if mid * mid == num:
                 return True
             elif mid * mid < num:
-                left = mid + 1
+                l = mid + 1
             else:
-                right = mid - 1
+                r = mid - 1
 
         return False
