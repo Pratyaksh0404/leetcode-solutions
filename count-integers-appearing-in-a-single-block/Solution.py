7ebdb521642cc100404daa63ac720641df7f644c
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-31 21:21:24 +0530

    feat: solve Count Integers Appearing in a Single Block

--- count-integers-appearing-in-a-single-block/Solution.py
+++ count-integers-appearing-in-a-single-block/Solution.py
@@ -1,6 +1,3 @@
 class Solution:
     def countSpecialIntegers(self, nums: list[int]) -> int:
-        return sum(
-            nums.index(x) + nums.count(x) - 1 == len(nums) - 1 - nums[::-1].index(x)
-            for x in set(nums)
-        )
+        return sum(nums.index(x) + nums.count(x) - 1 == len(nums) - 1 - nums[::-1].index(x) for x in set(nums))
