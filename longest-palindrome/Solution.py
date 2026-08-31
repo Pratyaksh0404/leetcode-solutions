7e8124becf8f4e79a82d51468a4ceef70abef0e8
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-31 21:08:47 +0530

    feat: solve Longest Palindrome

--- longest-palindrome/Solution.py
+++ longest-palindrome/Solution.py
@@ -1,17 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-
         cs = set()
         ans = 0
-        
         for i in s:
             if i in cs:
                 cs.remove(i)
                 ans += 2
             else:
                 cs.add(i)
-        
         if cs:
             ans += 1
-        
         return ans
