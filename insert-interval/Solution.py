856654314dc9702cc5950d02c80b7fd4abd832cd
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-01 19:25:18 +0530

    feat: solve Insert Interval

--- insert-interval/Solution.py
+++ insert-interval/Solution.py
@@ -1,19 +1,18 @@
 class Solution(object):
     def insert(self, ii, new):
-        m = []
+        ans = []
         i = 0
-
         while i < len(ii) and ii[i][1] < new[0]:
-            m.append(ii[i])
+            ans.append(ii[i])
             i += 1
         
         while i < len(ii) and ii[i][0] <= new[1]:
             new = [min(new[0], ii[i][0]), max(new[1], ii[i][1])]
             i += 1
-        m.append(new)
+        ans.append(new)
         
         while i < len(ii):
-            m.append(ii[i])
+            ans.append(ii[i])
             i += 1
         
-        return m
+        return ans
