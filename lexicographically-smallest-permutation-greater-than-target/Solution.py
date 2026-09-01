fe110623a91594e7e960668a79195341103a0e35
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-01 12:07:53 +0530

    feat: solve Lexicographically Smallest Permutation Greater Than Target

--- lexicographically-smallest-permutation-greater-than-target/Solution.py
+++ lexicographically-smallest-permutation-greater-than-target/Solution.py
@@ -4,6 +4,7 @@ class Solution:
         a,b = s,target
         n = len(a)
         c = [0]*26
+        
         for i in a:
             c[ord(i)-97] += 1
         t = tuple(c)
@@ -13,21 +14,19 @@ class Solution:
             if i==n:
                 return "" if g else None
             l = list(ct)
-
             for j in range(26):
                 if l[j]>0:
                     if not g and j<ord(b[i])-97:
                         continue
-
                     ng = g or (j>ord(b[i])-97)
                     l[j] -= 1
                     r = solve(i+1,tuple(l),ng)
                     l[j] += 1
-
                     if r is not None:
                         return chr(97+j) + r
 
             return None
+
         ans = solve(0,t,False)
         return ans or ""
         
