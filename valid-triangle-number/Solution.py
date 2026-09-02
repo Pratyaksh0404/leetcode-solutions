d1801ff00509d6d3b98a92d9218c62c15aecd9fb
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-02 10:21:02 +0530

    feat: solve Valid Triangle Number

--- valid-triangle-number/Solution.py
+++ valid-triangle-number/Solution.py
@@ -2,14 +2,13 @@ class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
         n = len(nums)
-        count = 0
-
+        ans = 0
         for i in range(n - 1, -1, -1):
-            left, right = 0, i - 1
-            while left < right:
-                if nums[left] + nums[right] > nums[i]:
-                    count += right - left
-                    right -= 1
+            l, r = 0, i - 1
+            while l < r:
+                if nums[l] + nums[r] > nums[i]:
+                    ans += r - l
+                    r -= 1
                 else:
-                    left += 1
-        return count
+                    l += 1
+        return ans
