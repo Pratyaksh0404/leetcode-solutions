0aeb9c07f2767c3fbad666613052eb186545ad7e
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-03 23:26:38 +0530

    feat: solve Simplify Path

---
+++ simplify-path/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def simplifyPath(self, p):
+        ans = []
+        p = p.split("/")
+        for i in p:
+            if ans and i == "..":
+                ans.pop()
+            elif i not in [".", "", ".."]:
+                ans.append(i)
+                
+        return "/" + "/".join(ans)
