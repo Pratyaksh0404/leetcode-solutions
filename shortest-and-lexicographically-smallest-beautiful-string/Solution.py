7e74a8ed359d9aff3f89210cd6017652b748877a
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-04 09:19:10 +0530

    feat: solve Shortest and Lexicographically Smallest Beautiful String

--- shortest-and-lexicographically-smallest-beautiful-string/Solution.py
+++ shortest-and-lexicographically-smallest-beautiful-string/Solution.py
@@ -4,11 +4,11 @@ class Solution:
         for i in range(len(s)):
             if s[i] == '0': 
                 continue
-            ones = 0
+            a = 0
             for j in range(i, len(s)):
                 if s[j] == '1':
-                    ones += 1
-                if ones == k:
+                    a += 1
+                if a == k:
                     sub = s[i:j+1]
                     if not ans or len(sub) < len(ans) or (len(sub) == len(ans) and sub < ans):
                         ans = sub
