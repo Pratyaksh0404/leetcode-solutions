89db2ad3e97235dc9b634fab8d4881060bb73b5c
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-07 14:00:31 +0530

    feat: solve Find Right Interval

--- find-right-interval/Solution.py
+++ find-right-interval/Solution.py
@@ -1,21 +1,11 @@
+import bisect
+
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        s = sorted((interval[0], i) for i, interval in enumerate(intervals))
-        n = len(intervals)
-        ans = []
-
-        for _, end in intervals:
-            left, right = 0, n - 1
-            ind = -1
-
-            while left <= right:
-                mid = (left + right) // 2
-                if s[mid][0] >= end:
-                    ind = s[mid][1]  
-                    right = mid - 1 
-                else:
-                    left = mid + 1 
-
-            ans.append(ind)
-
-        return ans
+        s = sorted((inter[0], i) for i, inter in enumerate(intervals))
+        n = len(s)
+        
+        return [
+            s[idx][1] if (idx := bisect.bisect_left(s, (end,))) < n else -1 
+            for _, end in intervals
+        ]
