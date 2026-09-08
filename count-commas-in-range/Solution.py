affc3833906a6c3a36679a7e331495c7bac6c8e0
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-08 06:43:53 +0530

    feat: solve Count Commas in Range

--- count-commas-in-range/Solution.py
+++ count-commas-in-range/Solution.py
@@ -3,10 +3,11 @@ class Solution:
         ans = 0
         curr = 1000
         c = 1
-        while n>=curr:
+        while n >= curr:
             next = curr*1000
-            num = min(n,next-1)-curr+1
+            num = min(n,next - 1) - curr + 1
             ans += num*c
             curr = next
             c += 1
+
         return ans
