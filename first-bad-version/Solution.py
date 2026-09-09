3fc673ec149f43cd40b64ceedcd79df0989561c9
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-09 18:12:47 +0530

    feat: solve First Bad Version

---
+++ first-bad-version/Solution.py
@@ -0,0 +1,14 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n):
+        l, r = 0, n
+        while l <= r:
+            mid = l + (r - l) // 2
+            if isBadVersion(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return l
