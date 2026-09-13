5bedf21c19201ac16add33d8408b318f2fafb170
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-13 10:20:14 +0530

    feat: solve Count Shadow Pairs I

--- count-shadow-pairs-i/Solution.py
+++ count-shadow-pairs-i/Solution.py
@@ -1,18 +1,16 @@
+from bisect import bisect_left
+
 class Solution:
     def shadowPairs(self, nums: list[int]) -> int:
         v = nums  
-        n, ans = len(v), 0
-        r, stk, last, cnt = [n] * n, [], {}, [0] * n
+        st = []
+        ans = 0
+        pop = st.pop
         
-        for i, x in enumerate(v):
-            while stk and v[stk[-1]] > x:
-                r[stk.pop()] = i
-            stk.append(i)
-
-        for i in range(n - 1, -1, -1):
-            nxt = last.get(v[i], n)
-            cnt[i] = 1 + (cnt[nxt] if nxt < r[i] else 0)
-            last[v[i]] = i
-            ans += r[i] - i - cnt[i]
-
+        for x in v:
+            ans += bisect_left(st, x)
+            while st and st[-1] > x:
+                pop()
+            st.append(x)
+            
         return ans
