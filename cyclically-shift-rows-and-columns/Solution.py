f44f0ba0428fe84dde93390b7b970f546eccad5a
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-13 10:04:27 +0530

    feat: solve Cyclically Shift Rows and Columns

--- cyclically-shift-rows-and-columns/Solution.py
+++ cyclically-shift-rows-and-columns/Solution.py
@@ -1,3 +1,15 @@
 class Solution:
     def cyclicShift(self, n: int, grid: list[list[int]], rowShift: list[int], colShift: list[int]) -> list[list[int]]:
-        return [[grid[(i+colShift[j])%n][(j+rowShift[(i+colShift[j])%n])%n] for j in range(n)] for i in range(n)]
+        shift = []
+        for i in range(n):
+            k = rowShift[i] % n
+            shift.append(grid[i][k:] + grid[i][:k])
+            
+        ans = [[0] * n for _ in range(n)]
+        for j in range(n):
+            k = colShift[j] % n
+            for i in range(n):
+                row = (i + k) % n
+                ans[i][j] = shift[row][j]
+                
+        return ans
