24a6465ae271143c2eadc1b4f96adf2976f23b27
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-15 16:23:52 +0530

    feat: solve First Missing Positive

---
+++ first-missing-positive/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def firstMissingPositive(self, arr: List[int]) -> int:
+        n = len(arr)
+        for i in range(n):
+            while 1<=arr[i]<=n and arr[arr[i]-1] != arr[i]:
+                arr[arr[i]-1], arr[i] = arr[i], arr[arr[i]-1]
+        
+        for i in range(n):
+            if arr[i] != i+1:
+                return i+1
+        return n+1
