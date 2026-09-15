c4f90b7688067106ee67e51fddf5abbf60c4ef2f
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-15 16:29:30 +0530

    feat: solve Set Matrix Zeroes

---
+++ set-matrix-zeroes/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows, cols = len(matrix), len(matrix[0])
+        cells = set()
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    cells.add((r, c))
+        
+        for r, c in cells:
+            matrix[r] = [0] * cols
+            for row in matrix:
+                row[c] = 0
