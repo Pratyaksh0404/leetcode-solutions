c9691da242f5650a4c6ddd9a78f1c1eab1c9f613
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-16 14:01:13 +0530

    feat: solve Mirror Reflection

---
+++ mirror-reflection/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:
+        while p % 2 == 0 and q % 2 == 0:
+            p //= 2
+            q //= 2
+        if p % 2 == 0:
+            return 2
+        return 0 if q % 2 == 0 else 1
