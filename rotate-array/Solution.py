c492312e76995468980ca8ef065ec89526add43c
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-19 12:03:57 +0530

    feat: solve Rotate Array

--- rotate-array/Solution.py
+++ rotate-array/Solution.py
@@ -1,21 +1,10 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+    def rotate(self, nums: list[int], k: int) -> None:
         n = len(nums)
-        k %= n
-        count = start = 0
-
-        while count < n:
-            current = start
-            prev = nums[start]
-            while True:
-                nxt = (current + k) % n
-                nums[nxt], prev = prev, nums[nxt]
-                current = nxt
-                count += 1
-
-                if start == current:
-                    break
-            start += 1
+        k = k % n
+        
+        if k != 0:
+            arr = nums[n-k:]
+            del nums[n-k:]
+            nums[0:0] = arr
+ 
