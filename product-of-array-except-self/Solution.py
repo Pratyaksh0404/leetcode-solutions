0c0e2fb51777f30a876fa1529534f8a3b6a6d4f9
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-19 12:04:27 +0530

    feat: solve Product of Array Except Self

---
+++ product-of-array-except-self/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [1] * n
+
+        for i in range(1, n):
+            ans[i] = ans[i - 1] * nums[i - 1]
+
+        prevSuffix = 1
+        for i in range(n - 2, -1, -1):
+            prevSuffix *= nums[i + 1]
+            ans[i] *= prevSuffix
+
+        return ans
