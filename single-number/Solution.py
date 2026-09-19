c8b76ec67bdbf1da5b2c444b3e679d24a9e2487d
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-19 11:24:41 +0530

    feat: solve Single Number

--- single-number/Solution.py
+++ single-number/Solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = 0
+        ans = 0
         for i in nums:
-            result ^= i
-        return result
+            ans ^= i
+            
+        return ans
