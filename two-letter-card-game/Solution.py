2c69e0a5609c42f42ed7fc968c0be3f15a9c90ea
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-19 17:38:52 +0530

    feat: solve Two-Letter Card Game

--- two-letter-card-game/Solution.py
+++ two-letter-card-game/Solution.py
@@ -2,20 +2,23 @@ class Solution:
     def score(self, cards: List[str], x: str) -> int:
         both = 0
         cnt1, cnt2 = collections.defaultdict(int), collections.defaultdict(int)
+        
         for c in cards:
-            if c == x + x:
+            if c[0] == x and c[1] == x:
                 both += 1
             elif c[0] == x:
                 cnt1[c[1]] += 1
             elif c[1] == x:
                 cnt2[c[0]] += 1
         
-        v1 = sorted(cnt1.values(), reverse=True)
-        v2 = sorted(cnt2.values(), reverse=True)
-        
-        def solve(cnt, have):
-            s = sum(cnt) + have
-            m = max(cnt[0] if cnt else 0, have)
-            return min(s // 2, s - m)
-        
-        return max(solve(v1, i) + solve(v2, both - i) for i in range(both + 1))
+        def solve(cnt):
+            vals = list(cnt.values())
+            s = sum(vals)
+            ma = max(vals) if vals else 0
+            return 0 if s < 2 else min(s // 2, s - ma)
+            
+        lr = sum(cnt1.values()) + sum(cnt2.values())
+        if both >= lr:
+            return lr
+            
+        return min(solve(cnt1) + solve(cnt2), (lr - both) // 2) + both
