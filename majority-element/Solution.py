2f4fd0ea8883899b421867122017007be7cb7766
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-19 11:26:39 +0530

    feat: solve Majority Element

--- majority-element/Solution.py
+++ majority-element/Solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        candidate = 0
+        c = 0
+        ans = 0
         
         for i in nums:
-            if count == 0:
-                candidate = i
+            if c == 0:
+                ans = i
             
-            if i == candidate:
-                count += 1
+            if i == ans:
+                c += 1
             else:
-                count -= 1
+                c -= 1
         
-        return candidate
+        return ans
