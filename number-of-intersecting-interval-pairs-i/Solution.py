d205b5266e09a9a8c12a7bb807082fb73f502adf
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-20 12:42:04 +0530

    feat: solve Number of Intersecting Interval Pairs I

--- number-of-intersecting-interval-pairs-i/Solution.py
+++ number-of-intersecting-interval-pairs-i/Solution.py
@@ -1,12 +1,3 @@
 class Solution:
     def countIntersectingIntervals(self, intervals: list[list[int]]) -> int:
-        ans = 0
-        n = len(intervals)
-        for i in range(n):
-            a_start, a_end = intervals[i]
-            for j in range(i + 1, n):
-                b_start, b_end = intervals[j]
-                if a_start <= b_end and b_start <= a_end:
-                    ans += 1
-                    
-        return ans
+        return sum(a[0]<=b[1] and b[0]<=a[1] for i,a in enumerate(intervals) for b in intervals[i+1:])
