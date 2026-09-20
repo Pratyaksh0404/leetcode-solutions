79b6506e928560d8b95efddadded1787e2d9ab0f
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-20 13:08:37 +0530

    feat: solve Number of Intersecting Interval Pairs II

--- number-of-intersecting-interval-pairs-ii/Solution.py
+++ number-of-intersecting-interval-pairs-ii/Solution.py
@@ -1,12 +1,6 @@
 from bisect import bisect_right
-
 class Solution:
-    def countIntersectingIntervals(self, intervals: list[list[int]]) -> int:
-        a = intervals
-        a.sort(key=lambda x: x[0])
-        s = [x[0] for x in a]
-        
-        return sum(
-            bisect_right(s, end) - (i + 1) 
-            for i, (_, end) in enumerate(a)
-        )
+    def countIntersectingIntervals(self, ii: list[list[int]]) -> int:
+        ii.sort(key=lambda x: x[0])
+        s = [x[0] for x in ii]
+        return sum(bisect_right(s,ii[i][1])-(i+1) for i in range(len(ii)))
