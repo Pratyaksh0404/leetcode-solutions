8588b179c15ea2b728b41209ef137d79ae57e2a7
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-25 23:08:44 +0530

    feat: solve Lexicographically Largest Power Array

--- lexicographically-largest-power-array/Solution.py
+++ lexicographically-largest-power-array/Solution.py
@@ -1,29 +1,38 @@
 class Solution:
     def largestPower(self, nums: list[int]) -> list[int]:
-        c = [0]*32768
-        for x in nums:
-            c[x] += 1
-        for i in range(15):
-            for m in range(32768):
-                if not (m & (1<<i)):
-                    c[m] += c[m | (1<<i)]
-        p = [0]*15
-
-        def check(p):
-            for v in set(p) | {len(nums)}:
-                if v and c[sum(1<<j for j in range(15) if p[14-j]>=v)] < v:
-                    return False
-            return True
-
-        for b in range(14,-1,-1):
-            i,l,h,r = 14-b,0,len(nums),0
-            while l<=h:
-                m = (l+h)//2
-                p[i] = m
-                if check(p):
-                    r,l = m,m+1
+        grp = [nums]
+        ans = [0] * 15
+        
+        for b in range(14, -1, -1):
+            mask = 1 << b
+            curr = 0
+            nxt = []
+            f = False
+            
+            for g in grp:
+                if f:
+                    nxt.append(g)
                 else:
-                    h = m-1
-            p[i] = r
-        return p
-
+                    s1 = True
+                    for x in g:
+                        if not (x & mask):
+                            s1 = False
+                            break
+                    
+                    if s1:
+                        curr += len(g)
+                        nxt.append(g)
+                    else:
+                        f = True
+                        gg = [x for x in g if (x & mask)]
+                        gw = [x for x in g if not (x & mask)]
+                        curr += len(gg)
+                        if gg:
+                            nxt.append(gg)
+                        if gw:
+                            nxt.append(gw)
+                            
+            ans[14 - b] = curr
+            grp = nxt
+            
+        return ans
